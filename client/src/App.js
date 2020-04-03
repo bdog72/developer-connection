f0204227ae3be86561b8800f410caff02618ddde
Brian Bycynski <high5bri@hotmail.com>
2020-04-03 05:09:32 -0400

    trying to work out mongodb

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <h1>Hello</h1>
+        <h1>Hello Bozo</h1>
       </>
     );
   }
--- server.js
+++ server.js
@@ -2,20 +2,30 @@
 //
 const express = require('express');
 const app = express();
-
-app.get('/api/users', (req, res) => {
-  res.json([
-    {
-      id: 1,
-      username: 'Brian'
-    },
-    {
-      id: 2,
-      username: 'Molly Moo'
-    }
-  ]);
-});
+// const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3001;
-
 app.listen(port);
+
+// const { MongoClient } = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+
+// app.get('/api/users', (req, res) => {
+//   MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+//     client
+//       .db('Cars')
+//       .collection('items')
+//       .insertOne(
+//         {
+//           model: 'Mazda 3',
+//           year: 2010
+//         },
+//         (err, res) => {
+//           if (err) {
+//             console.log(`ERROR: ${err}`);
+//           }
+//         }
+//       );
+//     client.close();
+//   });
+// });
