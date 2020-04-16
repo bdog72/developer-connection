a074dfec4d410cb275d7c0783a1775cd9f964369
Brian Bycynski <high5bri@hotmail.com>
2020-04-16 03:39:23 -0400

    finished lecture 26

--- CarService/app.js
+++ CarService/app.js
@@ -3,14 +3,21 @@
 //
 const Car = require('./src/car/Car');
 const RacingCar = require('./src/car/RacingCar');
-
-// Object.prototype.displayInfo = function () {
-//   console.log('DISPLAYING OBJECT INFO!!!!!!');
-// };
+const readline = require('readline-sync');
+const i = require('./src/lib/i');
 
 function runApp() {
+  function displayOptions() {
+    console.log('Press "1" to add a car');
+    console.log('Press "2" to remove a car');
+    console.log('Press "3" to list all of the cars');
+    console.log('Press "exit" quit the program');
+  }
+
   const car = new Car({ brand: 'Mazda', type: 3, year: 2010 });
   const car2 = new Car({ type: 'Mustang', brand: 'Ford', year: 2019 });
+  const car3 = new Car({ type: 'M6', brand: 'BMW', year: 2018 });
+  const car4 = new Car({ type: 'C500', brand: 'Mercedes', year: 2020 });
 
   const racingCar = new RacingCar({
     category: 'F1',
@@ -19,10 +26,55 @@ function runApp() {
     year: 2020,
   });
 
-  racingCar.displayInfo();
-  car.displayInfo();
+  const cars = [car, car2, car3, car4, racingCar];
 
-  const cars = [car, car2, racingCar];
+  let appIsRunning = true;
+
+  displayOptions();
+
+  while (appIsRunning) {
+    let userInput = readline.question('Write Me Something: ').toLowerCase();
+
+    switch (userInput) {
+      case '1':
+        console.log('Creating Car');
+        break;
+      case '2':
+        console.log('Removing Car');
+        break;
+      case '3':
+        cars.forEach(function (car) {
+          car.displayInfo();
+        });
+        break;
+      case 'exit':
+        console.log('GoodBye Bozo');
+        appIsRunning = false;
+        break;
+      default:
+        console.log('Invalid Option');
+        break;
+    }
+
+    // if (userInput === '1') {
+    //   //
+    //   console.log('Creating Car');
+    // } else if (userInput === '2') {
+    //   //
+    //   console.log('Removing Car');
+    // } else if (userInput === '3') {
+    //   //
+    //   cars.forEach(function (car) {
+    //     car.displayInfo();
+    //   });
+    //   console.log('List all Cars');
+    // } else if (userInput === 'exit') {
+    //   console.log('GoodBye Bozo');
+    //   appIsRunning = false;
+    // } else {
+    //   console.log('Invalid Option');
+    // }
+  }
 }
 
 runApp();
---
+++ CarService/src/lib/i.js
@@ -0,0 +1,36 @@
+//
+//
+const i = (function () {
+  return {
+    forEach: function (list, callback) {
+      // console.log('Testing');
+      for (let i = 0; i < list.length; i++) {
+        callback(list[i]);
+      }
+    },
+
+    map: function (list, callback) {
+      let mappedList = [];
+
+      for (let i = 0; i < list.length; i++) {
+        let item = callback(list[i]);
+        mappedList.push(item);
+      }
+      return mappedList;
+    },
+
+    filter: function (list, callback) {
+      let filteredList = [];
+
+      for (let i = 0; i < list.length; i++) {
+        let canAdd = callback(list[i]);
+        if (canAdd) {
+          filteredList.push(list[i]);
+        }
+      }
+      return filteredList;
+    },
+  };
+})();
+
+module.exports = i;
