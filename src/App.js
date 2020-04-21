aca58290a604cb7b6743aa7dee3dc1e22ab73e38
Brian Bycynski <high5bri@hotmail.com>
2020-04-21 03:53:12 -0400

    finished section 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 //
 //
 import React from 'react';
+
 import Header from './components/shared/Header';
+import RentalHome from './pages/RentalHome';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <RentalHome />
     </div>
   );
 };
