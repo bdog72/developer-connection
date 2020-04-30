ccf6bc2d491409bbed3737bada496d7f474b0bd6
Brian Bycynski <high5bri@hotmail.com>
2020-04-30 04:11:11 -0400

    finished lecture 91

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-
 import React from 'react';
 import Header from './components/shared/Header';
 import Routes from './Routes';
 
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
 import { initStore } from './store';
 
 const store = initStore();
 const App = () => {
-
   return (
     <Provider store={store}>
       <Router>
@@ -17,7 +16,7 @@ const App = () => {
         <Routes />
       </Router>
     </Provider>
-  )
-}
+  );
+};
 
 export default App;
