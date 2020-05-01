82ac6b0c0b931aa21490c5d3bf3f003d208d3af7
Brian Bycynski <high5bri@hotmail.com>
2020-05-01 03:47:13 -0400

    starting on auth

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,8 +1,11 @@
 //
 //
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, ErrorMessage } from 'react-hook-form';
 
+import FormError from './FormError';
+
+// eslint-disable-next-line
 const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const LoginForm = ({ onSubmit }) => {
@@ -13,43 +16,41 @@ const LoginForm = ({ onSubmit }) => {
       <div className="form-group">
         <label htmlFor="email">Email</label>
         <input
-          ref={register({ required: true, pattern: EMAIL_PATTERN })}
+          ref={register({
+            required: 'Email is required',
+            pattern: { value: EMAIL_PATTERN, message: 'Invalid Email format' },
+          })}
           name="email"
           type="email"
           className="form-control"
           id="email"
         />
-        {errors.email && (
-          <div className="alert alert-danger">
-            {errors.email.type === 'required' && <span>Email is required</span>}
-            {errors.email.type === 'pattern' && (
-              <span>Not valid email format</span>
-            )}
-          </div>
-        )}
+
+        {/* <ErrorMessage errors={errors} name="email">
+          {({ message }) => <p className="alert alert-danger">{message}</p>}
+        </ErrorMessage> */}
+        <FormError errors={errors} name="email">
+          {(message) => <p>{message}</p>}
+        </FormError>
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
         <input
           ref={register({
-            required: true,
-            minLength: 8,
+            required: 'Password is required',
+            minLength: {
+              value: 8,
+              message: 'Minimum length of password is 8 characters',
+            },
           })}
           type="password"
           name="password"
           className="form-control"
           id="password"
         />
-        {errors.password && (
-          <div className="alert alert-danger">
-            {errors.password.type === 'required' && (
-              <span>Password is required</span>
-            )}
-            {errors.password.type === 'minLength' && (
-              <span>Minimum length of password is 8 characters</span>
-            )}
-          </div>
-        )}
+        <ErrorMessage errors={errors} name="password">
+          {({ message }) => <p className="alert alert-danger">{message}</p>}
+        </ErrorMessage>
       </div>
       <button type="submit" className="btn btn-bwm-main">
         Submit
