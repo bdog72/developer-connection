e6bbc5b5db7976cf4a84ba1e1b2b968623c5097b
Brian Bycynski <high5bri@hotmail.com>
2020-05-25 03:47:20 -0400

    finished section 7

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
+import Alert from './components/layout/Alert';
+
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -22,6 +24,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
